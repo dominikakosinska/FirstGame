3f7e09b4a243cdf72d5698a6e53dbc1cc398aa18
Dominika <dominikakosinska@github.com>
2022-07-06 20:06:25 +0200

    Dodano punktację i pojawiający się losowo punkt

--- 02.Snake.py
+++ 02.Snake.py
@@ -1,6 +1,9 @@
 #import modułu; "pustego szablonu"
 import pygame
 
+#import modułu liczb pseudolosowych
+import random
+
 # szerokość i wysokość okna gry
 window = pygame.display.set_mode([600, 400])
 
@@ -16,6 +19,7 @@ pygame.font.init()
 #import czcionki ("Nazwa rodziny czcionek", rozmiar czcionki)
 font1 = pygame.font.SysFont("Calibri", 60)
 font2 = pygame.font.SysFont("Calibri", 30)
+font3 = pygame.font.SysFont("Calibri", 15)
 
 #wpisanie tekstu (zmienna = [czcionka].render("Tekst", wygładzenie krawędzi jako True lub False, [r, g, b] - kolor czcionki)
 text1 = font1.render("Welcome to the game!", True, [102, 0, 51])
@@ -28,6 +32,11 @@ szerokosc = 20
 wysokosc = 20
 krok = 20
 run = 0
+w = random.randrange(0, 580, 20)
+z = random.randrange(0, 380, 20)
+wszerokosc = 20
+zwysokosc = 20
+wynik = 0
 
 #ustawienie zmiennej logicznej
 running = True
@@ -84,10 +93,22 @@ while running:
 
         #czyszczenie ekranu
         window.fill(tlo)
+
+        #prezentacja wyniku
+        label = font3.render('Wynik: ' + str(wynik), True, (0, 0, 0))
+        window.blit(label, [0, 0])
+
         #rysowanie prostąkąta
         pygame.draw.rect(window, (0, 0, 0), (x, y, szerokosc, wysokosc))
+        pygame.draw.rect(window, (255, 0,0), (w, z, wszerokosc, zwysokosc))
+
         #odświeżanie ekranu
         pygame.display.update()
+
+        if x == w and y == z:
+            wynik += 1
+            w = random.randrange(0, 580, 20)
+            z = random.randrange(0, 380, 20)
 
 #zakończenie zadania
 pygame.quit()
